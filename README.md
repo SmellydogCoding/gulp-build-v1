87d90cde532804d12af249a0500cfc69fb760f25
SmellydogCoding <daniel.mcneil@outlook.com>
2016-12-04 18:05:18 -0500

    refactoring and code cleanup

---
+++ README.md
@@ -0,0 +1,4 @@
+# GULP-build-v1
+Full Stack JavaScript Techdegree Project 8
+
+Use Gulp to prepare a website for production
--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,16 @@
-const gulp = require('gulp');
-const concat = require('gulp-concat');
-const maps = require('gulp-sourcemaps');
-const uglify = require('gulp-uglify');
-const rename = require('gulp-rename');
-const del = require('del');
-const sass = require('gulp-sass');
+const gulp      = require('gulp');
+const concat    = require('gulp-concat');
+const maps      = require('gulp-sourcemaps');
+const uglify    = require('gulp-uglify');
+const rename    = require('gulp-rename');
+const del       = require('del');
+const sass      = require('gulp-sass');
 const converter = require('sass-convert');
-const csso = require('gulp-csso');
-const imagemin = require('gulp-imagemin');
-const eslint = require('gulp-eslint');
+const csso      = require('gulp-csso');
+const imagemin  = require('gulp-imagemin');
+const eslint    = require('gulp-eslint');
 
+// linter for the .js files
 gulp.task('lint', () => {
   return gulp.src('js/**/*.js')
   .pipe(eslint({
@@ -22,7 +23,8 @@ gulp.task('lint', () => {
   .pipe(eslint.failAfterError());
 });
 
-gulp.task('concatJS', gulp.series('lint', function(done) {
+// concatinates .js files, runs after linter finishes
+gulp.task('concatJS', gulp.series('lint', () => {
     return gulp.src(['js/**/*.js'])
     .pipe(maps.init())
     .pipe(concat('all.js'))
@@ -30,14 +32,16 @@ gulp.task('concatJS', gulp.series('lint', function(done) {
     .pipe(gulp.dest('dist/scripts'));
 }));
 
-gulp.task('minifyJS', gulp.series('concatJS', function(done) {
+// minifies concatinated .js file, runs after concatinator finishes
+gulp.task('minifyJS', gulp.series('concatJS', () => {
   return gulp.src('dist/scripts/all.js')
     .pipe(uglify())
     .pipe(rename('all.min.js'))
     .pipe(gulp.dest('dist/scripts'));
 }));
 
-gulp.task('sassconvert', function () {
+// converts all .sass files to .scss (compiler fails on .sass files)
+gulp.task('sassconvert', () => {
   return gulp.src('sass/**/*+(sass|scss)')
     .pipe(converter({
       from: 'sass',
@@ -47,7 +51,8 @@ gulp.task('sassconvert', function () {
     .pipe(gulp.dest('sasstemp'));
 });
 
-gulp.task('sasscompile', gulp.series('sassconvert', function() {
+// compile sass to css, runs after sass converter finishes
+gulp.task('sasscompile', gulp.series('sassconvert', () => {
   return gulp.src("sasstemp/global.scss")
       .pipe(rename('all.scss'))
       .pipe(maps.init())
@@ -56,35 +61,44 @@ gulp.task('sasscompile', gulp.series('sassconvert', function() {
       .pipe(gulp.dest('dist/styles'));
 }));
 
-gulp.task('sassminify', gulp.series('sasscompile', function () {
+// minifies the css file, runs after the sass compiler finishes
+gulp.task('sassminify', gulp.series('sasscompile', () => {
     return gulp.src('dist/styles/all.css')
         .pipe(csso())
         .pipe(rename('all.min.css'))
         .pipe(gulp.dest('dist/styles'));
 }));
 
-gulp.task('scripts', gulp.series('minifyJS', function(done) {
+// runs the lint, concatJS, and minifyJS tasks
+gulp.task('scripts', gulp.series('minifyJS', (done) => {
   del(['dist/scripts/all.js']);
   done();
 }));
 
-gulp.task('styles', gulp.series('sassminify', function(done) {
+// runs the sassconvert, sasscompile, and sassminify tasks
+gulp.task('styles', gulp.series('sassminify', (done) => {
   del(['dist/styles/all.css','sasstemp']);
   done();
 }));
 
+// optimizes images
 gulp.task('images', () =>
     gulp.src('images/*')
         .pipe(imagemin())
         .pipe(gulp.dest('dist/content'))
 );
 
-gulp.task('clean', function(done) {
+// deletes the dist folder
+gulp.task('clean', (done) => {
   del(['dist']);
   done();
 });
 
-gulp.task('build', gulp.series('clean', 'scripts', 'styles', 'images', function(done) {
+// runs the scripts, styles, and images tasks in parallel
+gulp.task('buildtasks', gulp.parallel('scripts', 'styles', 'images'));
+
+// runs the clean task, runs the buildtasks tasks after clean finishes
+gulp.task('build', gulp.series('clean', 'buildtasks', (done) => {
   done();
 }));
 
