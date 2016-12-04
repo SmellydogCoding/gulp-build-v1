f178a905ab6be46e172ba3018b7bf514c7ac9a3d
SmellydogCoding <daniel.mcneil@outlook.com>
2016-12-04 13:07:48 -0500

    gulp styles task

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,9 @@ const maps = require('gulp-sourcemaps');
 const uglify = require('gulp-uglify');
 const rename = require('gulp-rename');
 const del = require('del');
+const sass = require('gulp-sass');
+const converter = require('sass-convert');
+const csso = require('gulp-csso');
 
 gulp.task('concatJS', function() {
     return gulp.src(['js/**/*.js'])
@@ -22,6 +25,36 @@ gulp.task('minifyJS', ['concatJS'], function() {
 
 gulp.task('scripts', ['minifyJS'], function() {
   del(['dist/scripts/all.js']);
+});
+
+gulp.task('sassconvert', function () {
+  return gulp.src('sass/**/*+(sass|scss)')
+    .pipe(converter({
+      from: 'sass',
+      to: 'scss',
+      rename: true
+    }))
+    .pipe(gulp.dest('sasstemp'));
+});
+
+gulp.task('sasscompile', ['sassconvert'], function() {
+  return gulp.src("sasstemp/global.scss")
+      .pipe(rename('all.scss'))
+      .pipe(maps.init())
+      .pipe(sass())
+      .pipe(maps.write('./'))
+      .pipe(gulp.dest('dist/styles'));
+});
+
+gulp.task('sassminify', ['sasscompile'], function () {
+    return gulp.src('dist/styles/all.css')
+        .pipe(csso())
+        .pipe(rename('all.min.css'))
+        .pipe(gulp.dest('dist/styles'));
+});
+
+gulp.task('styles', ['sassminify'], function() {
+  del(['dist/styles/all.css','sasstemp']);
 });
 
 gulp.task('clean', function() {
